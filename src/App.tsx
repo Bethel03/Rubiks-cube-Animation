dd6a8848b3b5648facfc3ea0cb23fce4ef497fc8
Bethel03 <bethelkens9@gmail.com>
2026-06-08 22:12:37 +0200

    FIX: React import in App.tsx remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment, ContactShadows } from '@react-three/drei';
 import { Cube, type CubeRef } from './components/Cube';
